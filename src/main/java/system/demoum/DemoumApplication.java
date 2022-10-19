7aad17c7b1a574b3c802d6586f7c873bd13a67db
= <gabrielvalengasilva412@gmail.com>
2022-10-18 21:14:24 -0300

    commit um

---
+++ src/main/java/system/demoum/DemoumApplication.java
@@ -0,0 +1,13 @@
+package system.demoum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoumApplication.class, args);
+	}
+
+}
---
+++ src/main/java/system/demoum/Repository.java
@@ -0,0 +1,9 @@
+package system.demoum;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<Cliente, Long> {
+
+
+
+}
